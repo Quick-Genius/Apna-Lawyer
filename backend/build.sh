0744aea87223499c2bca1e27b732b6c06fed319e
Quick-Genius <singhharshgood@gmail.com>
2025-09-12 13:20:01 +0530

    fixed some deployement issues

--- backend/build.sh
+++ backend/build.sh
@@ -4,10 +4,14 @@ set -o errexit
 
 # Upgrade pip and install build dependencies first
 python -m pip install --upgrade pip
-pip install wheel setuptools
+python -m pip install --upgrade wheel setuptools
 
-# Install requirements
+# Force use of binary packages when available (avoid building from source)
+pip install --only-binary :all: Pillow==10.2.0
+
+# Install remaining requirements
 pip install -r requirements.txt
 
 # Run Django migrations
+python manage.py collectstatic --noinput
 python manage.py migrate
